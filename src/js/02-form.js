c761b6a8f0532fa9205a809e635090585f4c9f6a
TarikSanich <141185095+TarikSanich@users.noreply.github.com>
2024-01-12 12:32:33 +0200

    7

--- src/js/02-form.js
+++ src/js/02-form.js
@@ -1,43 +1,43 @@
-// import debounce from 'debounce';
+import debounce from 'debounce';
 
-// const form = document.querySelector('.feedback-form');
-// const LOCAL_FEEDBACK_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+const LOCAL_FEEDBACK_KEY = 'feedback-form-state';
 
-// try {
-//   const initialFormData =
-//     JSON.parse(localStorage.getItem(LOCAL_FEEDBACK_KEY)) || {};
+try {
+  const initialFormData =
+    JSON.parse(localStorage.getItem(LOCAL_FEEDBACK_KEY)) || {};
 
-//   Array.from(form.elements).forEach(element => {
-//     const storageValue = initialFormData[element.name];
-//     if (storageValue) {
-//       element.value = initialFormData[element.name];
-//     }
-//   });
-// } catch (e) {
-//   console.error('LOCAL STORAGE PARSE ERROR');
-// }
+  Array.from(form.elements).forEach(element => {
+    const storageValue = initialFormData[element.name];
+    if (storageValue) {
+      element.value = initialFormData[element.name];
+    }
+  });
+} catch (e) {
+  console.error('LOCAL STORAGE PARSE ERROR');
+}
 
-// form.addEventListener(
-//   'input',
-//   debounce(() => {
-//     const formData = new FormData(form);
-//     const formObject = {};
+form.addEventListener(
+  'input',
+  debounce(() => {
+    const formData = new FormData(form);
+    const formObject = {};
 
-//     formData.forEach((value, key) => {
-//       formObject[key] = value.trim();
-//     });
+    formData.forEach((value, key) => {
+      formObject[key] = value.trim();
+    });
 
-//     localStorage.setItem(LOCAL_FEEDBACK_KEY, JSON.stringify(formObject));
-//   }, 300)
-// );
+    localStorage.setItem(LOCAL_FEEDBACK_KEY, JSON.stringify(formObject));
+  }, 300)
+);
 
-// form.addEventListener('submit', event => {
-//   event.preventDefault();
+form.addEventListener('submit', event => {
+  event.preventDefault();
 
-//   console.log({
-//     email: form.elements.email.value,
-//     message: form.elements.message.value,
-//   });
+  console.log({
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  });
 
   localStorage.removeItem(LOCAL_FEEDBACK_KEY);
   form.reset();
